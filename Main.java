6a1434d28e3219b9982a88249094c57919cc271c
harsha2531 <harshanimeda@gmail.com>
2024-01-24 14:13:46 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+class Student {
+	
+	double prf;
+	double dbm;
+	
+		//Illegal
+		//Illegal
+	
+	public Student() {
+        prf = 76;
+		System.out.println(prf);
+	}	
+}
+
+public class Main {
+	public static void main(String args[]) {
+		Student s1 = new Student();
+	}
+}
